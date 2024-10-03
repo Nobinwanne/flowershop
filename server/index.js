0c1f4f1f09019d282af3975799b7e6d2a298e729
yamuna <yamunajayan@gmail.com>
2024-10-03 12:42:20 -0400

    update flowers.js file

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import flowersRoute from "./routes/flowers.js";
 
 const { PORT } = process.env;
 
@@ -10,6 +11,8 @@ app.use(cors());
 
 app.use(express.json());
 
-app.get("/", (_, res) => res.send("Welcome to /"));
+app.use("/flowers", flowersRoute);
+
+app.get("/", (_req, _res) => _res.send("Welcome to Flower Shop"));
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
