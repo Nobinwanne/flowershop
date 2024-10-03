fd0b26179197dbbee9ae60489ea21e8b9ef6a600
Nobinwanne <ndidiobinwanne@gmail.com>
2024-10-03 13:03:00 -0600

    create product component, product cards and add styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
 import "./App.css";
 import Hero from "./components/Hero/Hero";
 import Header from "./components/Header/Header";
+import Products from "./components/Products/Products";
 
 function App() {
-    return (
-        <>
-            <Header />
-            <Hero />
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Products />
+    </>
+  );
 }
 
 export default App;
