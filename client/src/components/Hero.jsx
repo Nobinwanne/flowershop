7a6557ddcd30e3272a069b636e8d43ea655c32c8
Nobinwanne <ndidiobinwanne@gmail.com>
2024-10-03 10:56:13 -0600

    add styling and icons

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import heroimage from "../assets/images/florist_image.webp";
 import herotulipimage from "../assets/images/florist_tulip.webp";
+import deliveryicon from "../assets/icons/delivery-icon.svg";
+import artistsicon from "../assets/icons/artists-icon.svg";
+import freshnessicon from "../assets/icons/freshness-icon.svg";
+import ordericon from "../assets/icons/order-icon.svg";
+
 import "../components/Hero.scss";
 
 function Hero() {
@@ -9,37 +14,53 @@ function Hero() {
       <section className="hero">
         <div className="hero-text">
           <p className="hero__offer">20% Off For New Members</p>
-          <h1 className="hero__headline">Your Special Day!</h1>
+          <h1 className="hero__header">Your Special Day!</h1>
           <p className="hero__copy">We are here for your Special Day</p>
           <a href="#" className="cta-button">
             Start Shopping
           </a>
         </div>
         <div className="hero-image">
-          <img src={herotulipimage} alt="Flowers" />
+          <img
+            className="hero-image__flower"
+            src={herotulipimage}
+            alt="Flowers"
+          />
         </div>
       </section>
 
       <section className="offers">
         <div className="offer">
-          <img src="free-delivery-icon.png" alt="Free Delivery" />
-          <h4>Free Delivery</h4>
-          <p>Free shipping around the world for all orders over $120</p>
+          <img className="offer__icon" src={deliveryicon} alt="Free Delivery" />
+          <h4 className="offer__copy">Free Delivery</h4>
+          <p className="offer__copy--phrase">
+            Free shipping around the world for all orders over $120
+          </p>
         </div>
         <div className="offer">
-          <img src="online-order-icon.png" alt="Online Order" />
-          <h4>Online Order</h4>
-          <p>Don't worry, you can order online by our site</p>
+          <img className="offer__icon" src={ordericon} alt="Online Order" />
+          <h4 className="offer__copy">Online Order</h4>
+          <p className="offer__copy--phrase">
+            Don't worry, you can order online by our site
+          </p>
         </div>
         <div className="offer">
-          <img src="freshness-icon.png" alt="Freshness" />
-          <h4>Freshness</h4>
-          <p>You have fresh flowers in every single order</p>
+          <img className="offer__icon" src={freshnessicon} alt="Freshness" />
+          <h4 className="offer__copy">Freshness</h4>
+          <p className="offer__copy--phrase">
+            You have fresh flowers in every single order
+          </p>
         </div>
         <div className="offer">
-          <img src="made-by-artists-icon.png" alt="Made by Artists" />
-          <h4>Made by Artists</h4>
-          <p>World-className flowers, made by artists</p>
+          <img
+            className="offer__icon"
+            src={artistsicon}
+            alt="Made by Artists"
+          />
+          <h4 className="offer__copy">Made by Artists</h4>
+          <p className="offer__copy--phrase">
+            World-className flowers, made by artists
+          </p>
         </div>
       </section>
     </>
